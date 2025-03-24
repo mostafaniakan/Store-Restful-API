3c2c5788ae96aebcfc44d672f46991e591c89432
Mostafa Niakan <mostafaniakan96@gmail.com>
2025-03-24 14:04:46 +0330

    Added 5 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI3NjQ3NQ/spectral.js?branch_slug=main&token=b1a936fe-6d27-4e70-85a4-3c80e59992e3";
+export default { extends: ruleset };
